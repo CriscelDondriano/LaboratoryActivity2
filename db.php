f890b9896c5635a8d7ddba57b1898be4573fc262
Steve <devomasteven13@gmail.com>
2024-10-06 14:42:05 +0800

    change port

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = ""; // Leave this empty if you haven't set a password for MySQL
 $dbname = "database1"; // Replace 'db' with the name of your database
-$port = 3307; // The default MySQL port is 3306
+$port = 3306; // The default MySQL port is 3306
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
